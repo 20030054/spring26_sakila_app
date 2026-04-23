e7885f1863f7b76362e33299764594f29ffdc87f
WaniaaM01 <waniaamasood5@gmail.com>
2026-04-23 17:16:31 +0500

    fix(config): add positive value validation for CONNECTION_TIMEOUT per review

--- config.py
+++ config.py
@@ -7,5 +7,5 @@
 import os
 
 MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
-CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+CONNECTION_TIMEOUT = max(1, int(os.environ.get('CONNECTION_TIMEOUT', '30')))
 HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
