f9fb10ebb236b53adce0b7ebb08c12407abab0d1
maazk <maazkb12@gmail.com>
2026-04-25 15:35:45 +0500

    fix(config): add input validation for timeout variables after PR review

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 # Author: Maaz Butt | Date: 25-04-2026
 # Team Member: TEAMMATE NAME | Date: 25-04-2026
 # Resolution: Kept sakila-db-server as host, included both new variables
+# Fix: Added input validation for timeout variables after PR review
 
 import os
 
@@ -11,5 +12,12 @@ class Config:
     MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
     SECRET_KEY = os.environ.get('SECRET_KEY', 'your-secret-key-here')
 
-    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
-    HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+    try:
+        CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+    except ValueError:
+        CONNECTION_TIMEOUT = 30
+
+    try:
+        HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+    except ValueError:
+        HEALTH_CHECK_INTERVAL = 10
