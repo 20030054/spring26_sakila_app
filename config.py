b1f6031dcc99a9fd30f5904409555b91770fbe29
Muhammad Qasim Tahir <qasim@Qasims-MacBook-Air.local>
2026-04-25 16:51:27 +0500

    fix: add try/except block for timeout parsing based on PR review

--- config.py
+++ config.py
@@ -1,16 +1,15 @@
-<<<<<<< HEAD
-# Author: Muhammad Qasim Tahir
-=======
-# Author: M. Saad Asim
->>>>>>> feature/add-healthcheck
+# Authors: Muhammad Qasim Tahir & M. Saad Asim
 # Date: April 25, 2026
+# Description: Combined configuration settings
 
 import os
 
-<<<<<<< HEAD
 MYSQL_HOST = 'sakila-db-server'
-CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
-=======
-MYSQL_HOST = 'db-primary'
+
+# Addressed PR review: added try/except for safer integer parsing
+try:
+    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+except ValueError:
+    CONNECTION_TIMEOUT = 30
+
 HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
->>>>>>> feature/add-healthcheck
