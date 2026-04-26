0e5ce84c7f97c992937f7dc77e2d43c8b914604d
Zakriya <zakriaahmed40@gmail.com>
2026-04-26 21:36:14 +0500

    Add Task C1 multi-stage CI pipeline

--- config.py
+++ config.py
@@ -6,10 +6,12 @@ import os
 Configuration settings for the Sakila Database connection.
 """
 
-MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
-MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
-MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+class Config:
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+    SECRET_KEY = os.environ.get('FLASK_SECRET_KEY', 'default-key-for-dev')
 
-CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
-HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+    HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
