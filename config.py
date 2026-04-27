69155713de7e4227150ef86108093cb4599c4fd2
HaniaAtta <attahania193@gmail.com>
2026-04-27 06:41:33 -0700

    docs(config): add inline documentation addressing review feedback

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')  # kept from featu
 MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
 MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
 MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
-CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))  # seconds before DB connection attempt times out
 HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
