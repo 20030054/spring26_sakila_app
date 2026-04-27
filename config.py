233eee0e7a4bb61b4a799129f841cdfa9030dfe0
Sooban Zaib Warraich <soobanwarraich786@gmail.com>
2026-04-27 23:32:07 +0500

    feat(config): add healthcheck interval and update host

--- config.py
+++ config.py
@@ -1,3 +1,31 @@
+# ============================================
+# Team Member: Sooban zaib warraich
+# Date: 2026-04-20
+# Description: Added healthcheck interval
+# ============================================
+
+import os
+
+MYSQL_HOST = os.environ.get('MYSQL_HOST', 'db-primary')
+MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', 'admin')
+MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+
+HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Name: Abdul Raffay Qasim
 # Name: Team Member
 # Date: 2026-04-23
